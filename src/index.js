e544004d8d2b635c40a71ecdbad65f42399a4dcb
ALEKSANDR <104900497+glinkinsasha@users.noreply.github.com>
2025-01-26 19:24:02 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { App } from './components/App';
-import './index.css';
-
-document.addEventListener('DOMContentLoaded', function () {    
-  document.body.appendChild(new App().$rootElement);
-});
